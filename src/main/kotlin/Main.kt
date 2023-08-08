ff341bea13aca014fc2ccce5c320684d74dff8eb
Luiz Felipe <lffm1994@gmail.com>
2023-08-07 21:10:29 -0300

    Utilizando estrutura de repetição

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,17 +1,42 @@
 fun main() {
     println("Bem vindo ao Bytebank!")
-    val titular: String = "Alex"
-    val numeroConta: Int = 1000
-    var saldo: Double = 0.0
-    saldo = 100 + 2.0
-    saldo += 200
-    saldo = 0.0
 
+    var i = 0
+    while(i < 5) {
 
-    println("titular $titular")
-    println("número da conta $numeroConta")
-    println("saldo $saldo")
+        val titular: String = "Alex $i"
+        val numeroConta: Int = 1000
+        var saldo: Double = i + 10.0
 
+        println("titular $titular")
+        println("número da conta $numeroConta")
+        println("saldo $saldo")
+        println()
+        i++
+    }
+
+    // For loop
+    /*
+    for (i in 5 downTo 1) {
+
+        if(i == 4) {
+            break
+        }
+
+        val titular: String = "Alex $i"
+        val numeroConta: Int = 1000
+        var saldo: Double = i + 10.0
+
+        println("titular $titular")
+        println("número da conta $numeroConta")
+        println("saldo $saldo")
+    }
+     */
+
+    // testeCondicoes(saldo)
+}
+
+fun testeCondicoes(saldo: Double) {
     // Estrutura condicional equivalentes 'if' e 'when'
     if (saldo > 0.0) {
         println("conta é positiva")
