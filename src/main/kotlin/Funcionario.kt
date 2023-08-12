7b39736efe402dcd9e7940c450bea408bb1a38f5
Luiz Felipe <lffm1994@gmail.com>
2023-08-11 23:56:47 -0300

    O problema de tipos diferentes

--- src/main/kotlin/Funcionario.kt
+++ src/main/kotlin/Funcionario.kt
@@ -1,9 +1,14 @@
 class Funcionario(
     val nome: String,
     val cpf: String,
-    val salario: Double
+    val salario: Double,
+    val tipo: Int // 0 funcionario, 1 gerente, 2 diretor
 ) {
     fun bonificacao(): Double {
-        return salario * 0.1
+        return when {
+            salario.equals(1) -> salario * 0.1
+            salario.equals(2) -> salario * 0.2
+            else -> salario * 0.3
+        }
     }
 }
