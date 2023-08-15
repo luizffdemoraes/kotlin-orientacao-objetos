707e4265145393ddf3e4ee541117aa2d1f8a1c70
Luiz Felipe <lffm1994@gmail.com>
2023-08-14 22:55:50 -0300

    Herdando de funcionário

--- src/main/kotlin/Diretor.kt
+++ src/main/kotlin/Diretor.kt
@@ -1,15 +1,14 @@
-class Gerente(
+class Diretor(
     nome: String,
     cpf: String,
     salario: Double,
-    val senha: Int
-) : Funcionario(nome = nome, cpf = cpf, salario = salario) {
-    /*
-    fun bonificacao(): Double {
-        return salario * 0.2
-    }
-     */
-
+    val senha: Int,
+    val plr: Double
+) : Funcionario(
+    nome = nome,
+    cpf = cpf,
+    salario = salario
+) {
     fun autentica(senha: Int): Boolean {
         if(this.senha == senha) {
             return true
